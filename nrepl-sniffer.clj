35a58a1bffa4d5a4e0cee9a163c242a5f5b46e4a
Laurent Petit <laurent.petit@gmail.com>
2014-11-30 22:47:38 +0100

    doc(nrepl-sniffer): docstring addition

--- nrepl-sniffer.clj
+++ nrepl-sniffer.clj
@@ -3,8 +3,9 @@
             [ccw.events      :as evt]
             [clojure.pprint  :as p]))
  
-(defn
-  collect-responses
+(defn collect-responses
+  "reacts to nrepl received responses by displaying them in an annoying but
+   didactic popup."
   [topic data]
   (e/info-dialog "nrepl response"
     (with-out-str (p/pprint data))))
