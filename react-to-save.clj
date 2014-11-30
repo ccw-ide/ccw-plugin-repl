7483e70cc239a074b438cdc6812fa9ed26377d9f
Laurent Petit <laurent.petit@gmail.com>
2014-11-30 23:40:06 +0100

    feat(react-to-save): enhanced example

--- react-to-save.clj
+++ react-to-save.clj
@@ -2,14 +2,14 @@
   (:require [ccw.eclipse     :as e]
             [ccw.events      :as evt]
             [clojure.pprint  :as p]
-            [ccw.api.repl    :as repl]))
+            [ccw.api.repl    :as repl]
+            [clojure.edn     :as edn]))
  
 (defn on-saved-file
   "react to saved files by sneakily sending a noop clojure comment via the repl
    (if any) containing the namespace that the editor found the file corresponds to."
-  [topic {:keys [namespace absolute-path repl] :as data}]
-  (e/info-dialog "Saved data info"
-    (with-out-str (p/pprint data)))
-  (repl/send repl (str ";; the namespace is " namespace) false true false))
+  [topic {repl :repl :keys [namespace absolute-path] :as data}]
+  (repl/send repl (str ";; the namespace is " namespace))
+  (repl/send repl (pr-str '(+ 3 4))))
 
 (evt/subscribe :ccw.editor.saved #'on-saved-file)
