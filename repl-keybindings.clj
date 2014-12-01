c6e6d6c5f341915fe7712591143c86991378f18c
Laurent Petit <laurent.petit@gmail.com>
2014-12-01 23:06:38 +0100

    feat(repl-keybindings): command for call Component's (user/reset) function

---
+++ repl-keybindings.clj
@@ -0,0 +1,8 @@
+(ns repl-keybindings
+  "Command(s) for working with the REPL"
+  (:require [ccw.api.repl :as repl]
+            [ccw.e4.dsl   :refer :all]))
+ 
+(defcommand reset-components "Reset Components System")
+(defkeybinding reset-components "Cmd+U R")
+(defhandler reset-components (fn [_] repl/send '(user/reset)))
